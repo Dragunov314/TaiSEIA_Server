a8b63a35e4c210f8117dcce253544b711b30e1b3
Dragunov314 <a22451648@gmail.com>
2018-06-05 16:42:47 +0800

    Upload program

---
+++ Server/Socket-Server/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Socket_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnListen = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.onlineStatusBox = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.portBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmdBox = new System.Windows.Forms.GroupBox();
+            this.cmdBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(164, 6);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(75, 23);
+            this.btnListen.TabIndex = 0;
+            this.btnListen.Text = "Listen";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Click += new System.EventHandler(this.btnListen_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(166, 28);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(6, 109);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(375, 140);
+            this.txtLog.TabIndex = 2;
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(6, 28);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(154, 23);
+            this.txtSend.TabIndex = 3;
+            // 
+            // onlineStatusBox
+            // 
+            this.onlineStatusBox.FormattingEnabled = true;
+            this.onlineStatusBox.Location = new System.Drawing.Point(247, 27);
+            this.onlineStatusBox.Name = "onlineStatusBox";
+            this.onlineStatusBox.Size = new System.Drawing.Size(134, 76);
+            this.onlineStatusBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(244, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Online Status";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // portBox
+            // 
+            this.portBox.Location = new System.Drawing.Point(105, 6);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(49, 23);
+            this.portBox.TabIndex = 7;
+            this.portBox.Text = "8080";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Server Port";
+            // 
+            // cmdBox
+            // 
+            this.cmdBox.Controls.Add(this.onlineStatusBox);
+            this.cmdBox.Controls.Add(this.label1);
+            this.cmdBox.Controls.Add(this.btnSend);
+            this.cmdBox.Controls.Add(this.txtSend);
+            this.cmdBox.Controls.Add(this.txtLog);
+            this.cmdBox.Enabled = false;
+            this.cmdBox.Location = new System.Drawing.Point(12, 35);
+            this.cmdBox.Name = "cmdBox";
+            this.cmdBox.Size = new System.Drawing.Size(392, 255);
+            this.cmdBox.TabIndex = 10;
+            this.cmdBox.TabStop = false;
+            this.cmdBox.Text = " Command Box";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 298);
+            this.Controls.Add(this.cmdBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.portBox);
+            this.Controls.Add(this.btnListen);
+            this.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.cmdBox.ResumeLayout(false);
+            this.cmdBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.CheckedListBox onlineStatusBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox portBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox cmdBox;
+    }
+}
+
---
+++ Server/Socket-Server/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Globalization;
+
+namespace Socket_Server
+{
+    public partial class Form1 : Form
+    {
+        TcpListener server; // Creates a TCP Listener To Listen to Any IPAddress trying to connect to the program with port 1980
+
+        int clientNum = 0;
+        List<TcpClient> clients;// Creates a TCP Client list
+
+        public Form1()
+        {
+            InitializeComponent();
+            clients = new List<TcpClient>();
+        }
+        
+        public void ServerReceive(ref List<TcpClient> tmp)
+        {
+            int i;
+            NetworkStream stream; //Creats a NetworkStream (used for sending and receiving data) 
+            int no = tmp.Count - 1;
+            stream = tmp[no].GetStream(); //Gets The Stream of The Connection
+            Thread t = new Thread(() => // Thread (like Timer)
+            {
+                try
+                {
+                    byte[] datalength = new byte[3]; // creates a new byte with length 4 ( used for receivng data's length)
+                    while ((i = stream.Read(datalength, 0, 3)) != 0)//Keeps Trying to Receive the Size of the Message or Data
+                    {
+                        // how to make a byte E.X byte[] examlpe = new byte[the size of the byte here] , i used BitConverter.ToInt32(datalength,0) cuz i received the length of the data in byte called datalength :D
+                        //byte[] data = new byte[BitConverter.ToInt32(datalength, 0)]; // Creates a Byte for the data to be Received On
+                        byte[] data = new byte[Convert.ToInt32(datalength[2])];
+                        for (int j = 0; j < 3; j++)
+                            data[j] = datalength[j];
+                        stream.Read(data, 3, data.Length-3); //Receives The Real Data not the Size
+                        string message = "";
+                        foreach (byte data_byte in data)
+                        {
+                            message = message + Convert.ToInt32(data_byte).ToString("X2") + " ";
+                        }
+                        this.Invoke((MethodInvoker)delegate // To Write the Received data
+                        {
+                            DateTime localDate = DateTime.Now;
+                            String timestamp = localDate.ToString(new CultureInfo("en-US")); 
+                            txtLog.AppendText(System.Environment.NewLine + timestamp+" | Client : " + message);
+                            // Encoding.Default.GetString(data); Converts Bytes Received to String
+                        });
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            });
+            t.IsBackground=true;
+            t.Start(); // Start the Thread
+
+        }
+        public void checkStatus()
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                onlineStatusBox.Items.Clear();
+            });
+
+            try
+            {
+                List<int> delete_index = new List<int>();
+                for (int i = 0; i < clients.Count; i++)
+                {
+                    if (clients[i].Connected == false)
+                    {
+                        delete_index.Add(i);
+                    }
+                }
+                for (int i = 0; i < delete_index.Count; i++)
+                {
+                    clients.RemoveAt(delete_index[i]);
+                }
+                for (int i = 0; i < clients.Count; i++)
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        onlineStatusBox.Items.Add("Client " + i, true);
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+            }
+            
+        }
+
+        public void ServerSend(string msg)
+        {
+            for (int i = 0; i < clients.Count; i++)
+            {
+                if (clients[i].Connected)
+                {
+                    try
+                    {
+                        NetworkStream stream; //Creats a NetworkStream (used for sending and receiving data) 
+                        stream = clients[i].GetStream(); //Gets The Stream of The Connection
+                        byte[] data; // creates a new byte without mentioning the size of it cuz its a byte used for sending
+                        data = Encoding.Default.GetBytes(msg); // put the msg in the byte ( it automaticly uses the size of the msg )
+                        int length = data.Length; // Gets the length of the byte data
+                        byte[] datalength = new byte[4]; // Creates a new byte with length of 4
+                        datalength = BitConverter.GetBytes(length); //put the length in a byte to send it
+                        stream.Write(datalength, 0, 4); // sends the data's length
+                        stream.Write(data, 0, data.Length); //Sends the real data
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+                }                
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //onlineStatusBox.Items.Add("123", false);
+            //onlineStatusBox.SetItemChecked(0, true);
+        }
+
+        private void btnListen_Click(object sender, EventArgs e)
+        {
+            server = new TcpListener(IPAddress.Any, Convert.ToInt32(portBox.Text));
+            server.Start(); // Starts Listening to Any IPAddress trying to connect to the program with port 1980
+            MessageBox.Show("Waiting For Connection");
+            Thread t = new Thread(() => // Creates a New Thread (like a timer)
+            {
+                while (true)
+                {
+                    clients.Add(server.AcceptTcpClient());  //Waits for the Client To Connect                 
+
+                    MessageBox.Show("Connected To Client");
+                    if (clients[clients.Count - 1].Connected) // If you are connected
+                    {
+                        clientNum++;
+                        ServerReceive(ref clients); //Start Receiving                                          
+                    }
+                }
+
+            });
+            t.IsBackground = true;
+            t.Start();
+
+            btnListen.Enabled = false;
+            cmdBox.Enabled = true;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            ServerSend(txtSend.Text); // uses the Function ClientSend and the msg as txtSend.Text            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            checkStatus();
+        }
+    }
+}
